eb5887232f33712d2c7cd3bfe725c8fddd751f18
martinco444 <comartin910@gmail.com>
2022-02-11 14:45:33 -0500

    entrega seguimiento 2

---
+++ Exception/Exception/NegativeNumberException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+public class NegativeNumberException extends Exception{
+	public NegativeNumberException() {
+		
+		super ("you must write a positive number");
+		
+		}	
+}
---
+++ Testing/model/RecipeTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RecipeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
